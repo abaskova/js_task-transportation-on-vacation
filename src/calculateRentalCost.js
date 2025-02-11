81f0c7db512fc9894a313a94619fc3bb8b9801e2
Sasha Baskova <alekbaskova@gmail.com>
2025-02-11 21:00:08 +0200

    add task JS

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rentCarCostPerDay = 40;
+  let totalCost = rentCarCostPerDay * days;
+
+  if (days >= 7) {
+    totalCost -= 50;
+  } else if (days >= 3 && days <= 6) {
+    totalCost -= 20;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
